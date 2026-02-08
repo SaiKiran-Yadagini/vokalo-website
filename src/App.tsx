dd5ce8b1dfd54dea62e0f3d40e5fba4a7a45359f
skychowdary <saikiran246816@gmail.com>
2026-02-08 19:59:57 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,12 @@ function SectionLoader() {
 function App() {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 1.5,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
-      wheelMultiplier: 1,
+      wheelMultiplier: 1.2,
       touchMultiplier: 2,
     });
 
